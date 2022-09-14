172822302892b7b3572becc44c82fbeb329ba8ca
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-09-14 09:14:59 -0400

    done

---
+++ lab_2-1.py
@@ -0,0 +1,17 @@
+# Creator JM 9/14/2022 cycle2 lab2
+
+print(123)
+print(20+20)
+print(2*4*6*8)
+#print(13/0)
+print(2 ** 3)
+print(+321)
+print(2020-4)
+print(9/5)
+print(3%10)
+print(2*2*2)
+print(-321)
+print(20+20)
+print(9//5)
+print(3%10)
+#print(0/0)
