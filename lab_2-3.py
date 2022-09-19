fb672e472a2139e0ba12fd122790a114102d342d
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-09-19 12:21:18 -0400

    added lab 2-3, 2-4, 2-5

---
+++ lab_2-3.py
@@ -0,0 +1,13 @@
+# Creator JM 9/14/22 lab2-3
+
+a = 2
+
+print(type(a)) # int
+
+print(type(float(a))) # float
+
+print(type(str(a))) # string
+
+a = True
+print(a==2) # == checks if statement is true
+# a doesn't equal 2 so it returns False
