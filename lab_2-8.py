ca0f94106da60b41a3d375280a32a1c86a7ea965
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-09-21 14:22:55 -0400

    added 2-8, 2-9

---
+++ lab_2-8.py
@@ -0,0 +1,15 @@
+# Creator JM 9/21/22 lab2-8 
+ 
+while True:
+    points = int(input('Enter amount of points:\n'))
+
+    if points >= 15:
+        print('you win a gold medal!')
+    else:
+        if points > 11:
+            print('you won a silver medal!')
+        else:
+            if points > 8:
+                print('you win a bronze')
+            else:
+                print('you arent a winner')
